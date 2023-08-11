b7f24c1a31469a3bfcb08be3b328bf7aacf2e679
huseyineren <huseyin.eren@finastech.com>
2023-08-11 11:11:56 +0300

    elk stack docker compose

---
+++ src/test/java/com/huseyineren/elk/SpringBootElkStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huseyineren.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElkStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
